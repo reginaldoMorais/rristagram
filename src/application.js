dab127dfa2641cd3a8108c27ebb69c7ab2a4def2
Reginaldo Morais <reginaldo.morais@b2wdigital.com>
2016-12-20 18:13:38 -0200

    Ajuste nos imports da aplicação.

--- src/application.js
+++ src/application.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// Import reducers
-import reducer from './reducers';
+// Import Stylecheets
+import '../assets/style.styl';
+//import '../assets/main.scss';
+//var css = require('!css!stylus!../assets/style.styl'); 
+
+// Import reducers and store
+//import reducer from './reducers';
+//import { store, history } from './store';
 
 // Import components
 import PhotoGrid from './components/PhotoGrid';
